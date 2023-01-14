ef4d7a9ff91f2e99fadc97cda04bc16f25251642
lucabriguglia <luca.briguglia@outlook.com>
2023-01-14 13:49:38 +0000

    WIP

--- example/Cledev.Example.Server/Handlers/GetCreateItemHandler.cs
+++ example/Cledev.Example.Server/Handlers/GetCreateItemHandler.cs
@@ -7,46 +7,48 @@ namespace Cledev.Example.Server.Handlers;
 
 public class GetCreateItemHandler : IQueryHandler<GetCreateItem, CreateItem>
 {
-    public Task<Result<CreateItem>> Handle(GetCreateItem query)
-    {
-        throw new NotImplementedException();
-    }
-    
-    public async Task<Result2<CreateItem>> Handle2T(GetCreateItem query)
+    public async Task<Result<CreateItem>> Handle(GetCreateItem query)
     {
         await Task.CompletedTask;
-
-        return Result2<CreateItem>.Ok(new CreateItem());
-        return Result2<CreateItem>.Fail(ErrorCodes.NotFound, "Item", $"Item not found");
 
         return new CreateItem();
-        return new Failure(ErrorCodes.NotFound, "Item", $"Item not found");
     }
     
-    public async Task<Result3<CreateItem>> Handle3T(GetCreateItem query)
-    {
-        await Task.CompletedTask;
+    //public async Task<Result2<CreateItem>> Handle2T(GetCreateItem query)
+    //{
+    //    await Task.CompletedTask;
 
-        return Result3<CreateItem>.Ok(new CreateItem());
-        return Result3<CreateItem>.Fail(ErrorCodes.NotFound, "Item", $"Item not found");
+    //    return Result2<CreateItem>.Ok(new CreateItem());
+    //    return Result2<CreateItem>.Fail(ErrorCodes.NotFound, "Item", $"Item not found");
+
+    //    return new CreateItem();
+    //    return new Failure(ErrorCodes.NotFound, "Item", $"Item not found");
+    //}
+    
+    //public async Task<Result3<CreateItem>> Handle3T(GetCreateItem query)
+    //{
+    //    await Task.CompletedTask;
+
+    //    return Result3<CreateItem>.Ok(new CreateItem());
+    //    return Result3<CreateItem>.Fail(ErrorCodes.NotFound, "Item", $"Item not found");
         
-        return new Success<CreateItem>(new CreateItem());
-        return new Failure(ErrorCodes.NotFound, "Item", $"Item not found");
+    //    return new Success<CreateItem>(new CreateItem());
+    //    return new Failure(ErrorCodes.NotFound, "Item", $"Item not found");
 
-        return new CreateItem();
-        return Array.Empty<IEvent>();
-    }
+    //    return new CreateItem();
+    //    return Array.Empty<IEvent>();
+    //}
 
-    public async Task<Result3> Handle3(GetCreateItem query)
-    {
-        await Task.CompletedTask;
+    //public async Task<Result3> Handle3(GetCreateItem query)
+    //{
+    //    await Task.CompletedTask;
 
-        return Result3.Ok();
-        return Result3.Fail(ErrorCodes.NotFound, "Item", $"Item not found");
+    //    return Result3.Ok();
+    //    return Result3.Fail(ErrorCodes.NotFound, "Item", $"Item not found");
 
-        return new Success();
-        return new Failure(ErrorCodes.NotFound, "Item", $"Item not found");
+    //    return new Success();
+    //    return new Failure(ErrorCodes.NotFound, "Item", $"Item not found");
         
-        return Array.Empty<IEvent>();
-    }
+    //    return Array.Empty<IEvent>();
+    //}
 }
--- src/Cledev.Core/Results/Results3.cs
+++ src/Cledev.Core/Results/Results3.cs
@@ -3,13 +3,13 @@ using OneOf;
 
 namespace Cledev.Core.Results;
 
-public sealed class Result3 : OneOfBase<Success, Failure>
+public sealed class Result : OneOfBase<Success, Failure>
 {
-    private Result3(OneOf<Success, Failure> input) : base(input) { }
+    private Result(OneOf<Success, Failure> input) : base(input) { }
 
-    public static implicit operator Result3(Success success) => new(success);
-    public static implicit operator Result3(Failure failure) => new(failure);
-    public static implicit operator Result3(IEvent[] events) => new(new Success(events));
+    public static implicit operator Result(Success success) => new(success);
+    public static implicit operator Result(Failure failure) => new(failure);
+    public static implicit operator Result(IEvent[] events) => new(new Success(events));
     
     public bool IsSuccess => IsT0;
     public bool IsFailure => IsT1;
@@ -19,22 +19,22 @@ public sealed class Result3 : OneOfBase<Success, Failure>
 
     public IEnumerable<IEvent> Events => AsT0.Events;
     
-    public static Result3 Ok(Success success) => new(success);
-    public static Result3 Ok(params IEvent[] events) => new(new Success(events));
-    public static Result3 Fail(string errorCode = ErrorCodes.Error, string? title = null, string? description = null) => new(new Failure(errorCode, title, description));
+    public static Result Ok(Success success) => new(success);
+    public static Result Ok(params IEvent[] events) => new(new Success(events));
+    public static Result Fail(string errorCode = ErrorCodes.Error, string? title = null, string? description = null) => new(new Failure(errorCode, title, description));
 
     public bool TryPickSuccess(out Success success, out Failure failure) => TryPickT0(out success, out failure);
     public bool TryPickFailure(out Failure failure, out Success success) => TryPickT1(out failure, out success);
 }
 
-public sealed class Result3<TResult> : OneOfBase<Success<TResult>, Failure>
+public sealed class Result<TResult> : OneOfBase<Success<TResult>, Failure>
 {
-    private Result3(OneOf<Success<TResult>, Failure> input) : base(input) { }
+    private Result(OneOf<Success<TResult>, Failure> input) : base(input) { }
 
-    public static implicit operator Result3<TResult>(Success<TResult> success) => new(success);
-    public static implicit operator Result3<TResult>(Failure failure) => new(failure);
-    public static implicit operator Result3<TResult>(TResult result) => new(new Success<TResult>(result));
-    public static implicit operator Result3<TResult>(IEvent[] events) => new(new Success<TResult>(events));
+    public static implicit operator Result<TResult>(Success<TResult> success) => new(success);
+    public static implicit operator Result<TResult>(Failure failure) => new(failure);
+    public static implicit operator Result<TResult>(TResult result) => new(new Success<TResult>(result));
+    public static implicit operator Result<TResult>(IEvent[] events) => new(new Success<TResult>(events));
     
     public bool IsSuccess => IsT0;
     public bool IsFailure => IsT1;
@@ -45,9 +45,9 @@ public sealed class Result3<TResult> : OneOfBase<Success<TResult>, Failure>
     public new TResult? Value => AsT0.Result;
     public IEnumerable<IEvent> Events => AsT0.Events;
 
-    public static Result3<TResult> Ok(Success<TResult> success) => new(success);
-    public static Result3<TResult> Ok(TResult result, params IEvent[] events) => new(new Success<TResult>(result, events));
-    public static Result3<TResult> Fail(string errorCode = ErrorCodes.Error, string? title = null, string? description = null) => new(new Failure(errorCode, title, description));
+    public static Result<TResult> Ok(Success<TResult> success) => new(success);
+    public static Result<TResult> Ok(TResult result, params IEvent[] events) => new(new Success<TResult>(result, events));
+    public static Result<TResult> Fail(string errorCode = ErrorCodes.Error, string? title = null, string? description = null) => new(new Failure(errorCode, title, description));
 
     public bool TryPickSuccess(out Success<TResult> success, out Failure failure) => TryPickT0(out success, out failure);
     public bool TryPickFailure(out Failure failure, out Success<TResult> success) => TryPickT1(out failure, out success);
