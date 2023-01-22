626012b6743888208228b626c1d195ba35d0b8e1
lucabriguglia <luca.briguglia@outlook.com>
2023-01-22 11:18:41 +0000

    Return failure instead of throwing an exception

--- src/Cledev.Core/Queries/QueryHandlerWrapper.cs
+++ src/Cledev.Core/Queries/QueryHandlerWrapper.cs
@@ -4,15 +4,15 @@ namespace Cledev.Core.Queries;
 
 internal class QueryHandlerWrapper<TQuery, TResult> : QueryHandlerWrapperBase<TResult> where TQuery : IQuery<TResult>
 {
-    public override Task<Result<TResult>> Handle(IQuery<TResult> query, IServiceProvider serviceProvider)
+    public override async Task<Result<TResult>> Handle(IQuery<TResult> query, IServiceProvider serviceProvider)
     {
         var handler = GetHandler<IQueryHandler<TQuery, TResult>>(serviceProvider);
 
         if (handler == null)
         {
-            throw new Exception($"Handler not found for query of type {typeof(TQuery)}");
+            return Result<TResult>.Fail(ErrorCodes.Error, title: "Handler not found", description: $"Handler not found for query of type {typeof(TQuery)}");
         }
 
-        return handler.Handle((TQuery)query);
+        return await handler.Handle((TQuery)query);
     }
 }
