fee5a3ee0b40da44cf3fa2347169e3ce3ba08c49
Luca Briguglia <luca.briguglia@outlook.com>
2023-01-25 15:18:27 +0000

    Update Failure.cs

--- src/Cledev.Core/Results/Failure.cs
+++ src/Cledev.Core/Results/Failure.cs
@@ -8,3 +8,11 @@ public static class ErrorCodes
     public const string NotFound = "NotFound";
     public const string Unauthorized = "Unauthorized";
 }
+
+//public static class FailureExtensions
+//{
+//    public static Failure WithTitle(this Failure failure, string title) => failure with { Title = title };
+//    public static Failure WithDescription(this Failure failure, string description) => failure with { Description = description };
+//    public static Failure WithDescription(this Failure failure, string description, IEnumerable<string> items) => failure with { Description = $"{description}: {string.Join(", ", items)}" };
+//    public static Failure WithValidationResult(this Failure failure, ValidationResult validationResult) => failure with { ValidationResult = validationResult };
+//}
